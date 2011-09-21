b1095057ce1f12e598cabe5733a84377ccaeed7f
Tom Densham <tom.densham@studiobonito.co.uk>
2011-09-21 10:15:25 +0100

    ADD: Include the site config class

---
+++ code/ContactUsSiteConfig.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Contact Us Module adds a page type to Silverstripe for creating contact forms and embedding google maps.
+ *
+ * @package		Silverstripe Contact Us Module
+ * @version		1.0
+ * @author		Tom Densham <tom.densham at studiobonito.co.uk>
+ * @license		Simplified BSD License
+ * @copyright	2011 Studio Bonito Ltd.
+ * @link		https://github.com/studiobonito/silverstripe-contactus
+ */
+
+/**
+ * ContactUsSiteConfig Class
+ * 
+ * @package		Silverstripe Contact Us Module
+ */
+class ContactUsSiteConfig extends DataObjectDecorator {
+	
+	public function extraStatics() {
+		return array(
+			'db' => array(
+				'ContactTelephone' => 'Varchar',
+				'ContactEmail' => 'Varchar'
+			)
+		);
+	}
+
+}
